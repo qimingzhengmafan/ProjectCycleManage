a07abf52de2fccac1741414585ffe571bb9b7e73
yx <HD_yanx@huadalasers.com>
2025-08-13 10:35:08 +0800

    加入测试项

---
+++ Console_Miniexceltest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using MiniExcelLibs;
+
+var regs = MiniExcel.Query<Person>("");
+
+public class Person
+{
+    public int 姓名 { get; set; }
+    public string 年纪 { get; set; }
+    public string 性别 { get; set; }
+}
