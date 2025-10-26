b9d588044c5c32e4894e73bb35b6c2a24e0b4ae2
DoctorWho <DoctorWho@bbc.com>
2025-10-26 16:08:28 +0800

    1

--- ProjectCycleManage/View/UserControls/InformationCard.xaml.cs
+++ ProjectCycleManage/View/UserControls/InformationCard.xaml.cs
@@ -27,6 +27,17 @@ namespace ProjectCycleManage.View.UserControls
 
 
 
+        public string InformationTag
+        {
+            get { return (string)GetValue(InformationTagProperty); }
+            set { SetValue(InformationTagProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InformationTag.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InformationTagProperty =
+            DependencyProperty.Register(nameof(InformationTag), typeof(string), typeof(InformationCard), new PropertyMetadata(null));
+
+
         public string InformationTypes
         {
             get { return (string)GetValue(InformationTypesProperty); }
@@ -35,7 +46,30 @@ namespace ProjectCycleManage.View.UserControls
 
         // Using a DependencyProperty as the backing store for InformationTypes.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty InformationTypesProperty =
-            DependencyProperty.Register(nameof(InformationTypes), typeof(string), typeof(InformationCard), new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(InformationTypes), typeof(string), typeof(InformationCard), new PropertyMetadata(null , OnInformationTypesChanged));
+
+        private static void OnInformationTypesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #region 文档
+
+        #endregion
+
+        #region 文档-OA
+
+        #endregion
+
+        #region 信息-项目负责人/项目跟进人
+
+        #endregion
+
+        #region 信息-填写内容
+
+        #endregion
+
+
 
 
     }
