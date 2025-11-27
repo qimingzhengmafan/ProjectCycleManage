76700064551a7db515a6118d1d01019671062983
DoctorWho <DoctorWho@bbc.com>
2025-11-27 21:07:27 +0800

    添加月度预算

---
+++ ProjectCycleManage/Migrations/20251127130409_FixChange22.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChange22 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AprilBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "4月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AugustBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "8月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "DecemberBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "12月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "FebruaryBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "2月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JanuaryBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "1月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JulyBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "7月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JuneBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "6月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "MarchBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "3月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "MayBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "5月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "NovemberBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "11月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "OctoberBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "10月预算");
+
+            migrationBuilder.AddColumn<double>(
+                name: "SeptemberBudget",
+                table: "AnnualBudgetTable",
+                type: "double",
+                nullable: true,
+                comment: "9月预算");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AprilBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "AugustBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "DecemberBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "FebruaryBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "JanuaryBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "JulyBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "JuneBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "MarchBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "MayBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "NovemberBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "OctoberBudget",
+                table: "AnnualBudgetTable");
+
+            migrationBuilder.DropColumn(
+                name: "SeptemberBudget",
+                table: "AnnualBudgetTable");
+        }
+    }
+}
--- ProjectCycleManage/Model/AnnualBudget.cs
+++ ProjectCycleManage/Model/AnnualBudget.cs
@@ -14,5 +14,41 @@ namespace ProjectCycleManage.Model
         public int Year { get; set; }
         [Comment("预算")]
         public int Budget { get; set; }
+        
+        [Comment("1月预算")]
+        public double? JanuaryBudget { get; set; }
+        
+        [Comment("2月预算")]
+        public double? FebruaryBudget { get; set; }
+        
+        [Comment("3月预算")]
+        public double? MarchBudget { get; set; }
+        
+        [Comment("4月预算")]
+        public double? AprilBudget { get; set; }
+        
+        [Comment("5月预算")]
+        public double? MayBudget { get; set; }
+        
+        [Comment("6月预算")]
+        public double? JuneBudget { get; set; }
+        
+        [Comment("7月预算")]
+        public double? JulyBudget { get; set; }
+        
+        [Comment("8月预算")]
+        public double? AugustBudget { get; set; }
+        
+        [Comment("9月预算")]
+        public double? SeptemberBudget { get; set; }
+        
+        [Comment("10月预算")]
+        public double? OctoberBudget { get; set; }
+        
+        [Comment("11月预算")]
+        public double? NovemberBudget { get; set; }
+        
+        [Comment("12月预算")]
+        public double? DecemberBudget { get; set; }
     }
 }
