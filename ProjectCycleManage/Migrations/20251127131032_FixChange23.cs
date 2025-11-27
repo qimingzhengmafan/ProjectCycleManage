061e31732dab4ffb13182c0376232acc80680f0e
DoctorWho <DoctorWho@bbc.com>
2025-11-27 21:11:23 +0800

    添加月度销售预测

---
+++ ProjectCycleManage/Migrations/20251127131032_FixChange23.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChange23 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterTable(
+                name: "SalesVolumeTables",
+                comment: "年度销售预测",
+                oldComment: "年度销售额")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: false,
+                comment: "销售预测",
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldComment: "销售额");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AprilSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "4月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AugustSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "8月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "DecemberSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "12月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "FebruarySalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "2月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JanuarySalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "1月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JulySalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "7月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "JuneSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "6月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "MarchSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "3月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "MaySalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "5月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "NovemberSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "11月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "OctoberSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "10月销售预测");
+
+            migrationBuilder.AddColumn<double>(
+                name: "SeptemberSalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: true,
+                comment: "9月销售预测");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AprilSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "AugustSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "DecemberSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "FebruarySalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "JanuarySalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "JulySalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "JuneSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "MarchSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "MaySalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "NovemberSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "OctoberSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.DropColumn(
+                name: "SeptemberSalesVolume",
+                table: "SalesVolumeTables");
+
+            migrationBuilder.AlterTable(
+                name: "SalesVolumeTables",
+                comment: "年度销售额",
+                oldComment: "年度销售预测")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SalesVolume",
+                table: "SalesVolumeTables",
+                type: "double",
+                nullable: false,
+                comment: "销售额",
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldComment: "销售预测");
+        }
+    }
+}
--- ProjectCycleManage/Model/SalesVolumeTable.cs
+++ ProjectCycleManage/Model/SalesVolumeTable.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProjectCycleManage.Model
 {
-    [Comment("年度销售额")]
+    [Comment("年度销售预测")]
     public class SalesVolumeTable
     {
         public int Id { get; set; }
@@ -17,10 +17,44 @@ namespace ProjectCycleManage.Model
         public int Year { get; set; }
 
 
-        [Comment("销售额")]
+        [Comment("销售预测")]
         public double SalesVolume { get; set; }
 
+        [Comment("1月销售预测")]
+        public double? JanuarySalesVolume { get; set; }
 
+        [Comment("2月销售预测")]
+        public double? FebruarySalesVolume { get; set; }
+
+        [Comment("3月销售预测")]
+        public double? MarchSalesVolume { get; set; }
+
+        [Comment("4月销售预测")]
+        public double? AprilSalesVolume { get; set; }
+
+        [Comment("5月销售预测")]
+        public double? MaySalesVolume { get; set; }
+
+        [Comment("6月销售预测")]
+        public double? JuneSalesVolume { get; set; }
+
+        [Comment("7月销售预测")]
+        public double? JulySalesVolume { get; set; }
+
+        [Comment("8月销售预测")]
+        public double? AugustSalesVolume { get; set; }
+
+        [Comment("9月销售预测")]
+        public double? SeptemberSalesVolume { get; set; }
+
+        [Comment("10月销售预测")]
+        public double? OctoberSalesVolume { get; set; }
+
+        [Comment("11月销售预测")]
+        public double? NovemberSalesVolume { get; set; }
+
+        [Comment("12月销售预测")]
+        public double? DecemberSalesVolume { get; set; }
 
     }
 }
