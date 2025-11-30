9f88ec202821945f9926a1192eab556395081340
DoctorWho <DoctorWho@bbc.com>
2025-11-30 21:55:31 +0800

    refactor(Charts): 清理注释掉的代码并更新区域描述
    移除ChartsVM.cs中已注释的图表系列和轴配置代码
    将Charts.xaml中的区域描述从"区域描述"更新为"项目进度"

--- ProjectCycleManage/ViewModel/ChartsVM.cs
+++ ProjectCycleManage/ViewModel/ChartsVM.cs
@@ -154,43 +154,6 @@ namespace ProjectCycleManage.ViewModel
 
             }
         }
-        //public ISeries[] Series { get; set; } =
-        //[
-        //    new ColumnSeries<double>
-        //    {
-        //        Name = "预算金额",
-        //        Values = [2000, 5000, 4000, 5000, 5000, 5000],
-        //        MaxBarWidth = 30,
-        //        Fill = new SolidColorPaint(new SKColor(76, 172, 250)),
-        //    },
-
-        //    new ColumnSeries<double>
-        //    {
-        //        Name = "实际支出",
-        //        Values = [3000, 1000, 6000, 5000, 5000, 5000],
-        //        MaxBarWidth = 30,
-        //        Fill = new SolidColorPaint(new SKColor(109, 203, 112)),
-        //    }
-        //];
-
-        //public Axis[] XAxes { get; set; } =
-        //[
-        //    new Axis
-        //    {
-        //        Labels = ["测试 1", "Category 2", "Category 3","Category 4", "Category 5", "Category 6","Category 7", "Category 8", "Category 9"],
-        //        LabelsRotation = 0,
-        //        SeparatorsPaint = new SolidColorPaint(new SKColor(200, 200, 200)),
-        //        SeparatorsAtCenter = false,
-        //        TicksPaint = new SolidColorPaint(new SKColor(35, 35, 35)),
-        //        TicksAtCenter = true,
-        //        // By default the axis tries to optimize the number of 
-        //        // labels to fit the available space, 
-        //        // when you need to force the axis to show all the labels then you must: 
-        //        ForceStepToMin = true,
-        //        MinStep = 1,
-        //        LabelsPaint = new SolidColorPaint(SKColors.Black){SKTypeface = SKFontManager.Default.MatchCharacter('汉')}, // 标签颜色
-        //    }
-        //];
         #endregion
 
 
