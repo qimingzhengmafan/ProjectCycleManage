77e11e1aabf5393466353c9b6e0089e7a2ff2413
DoctorWho <DoctorWho@bbc.com>
2025-11-30 20:09:44 +0800

    feat(设置视图): 添加人员管理和项目阶段管理功能
    - 在SettingView.xaml中新增两个卡片按钮，分别绑定到PersonalVMCardCommand和ProjectStageVMCardCommand
    - 添加PersonManagView和ProjectStageView的显示逻辑，通过Visibility属性控制
    - 在SettingVM.cs中新增相关属性和命令处理逻辑
    - 清理PersonManagView.xaml中未使用的事件处理器

--- ProjectCycleManage/ViewModel/SettingVM.cs
+++ ProjectCycleManage/ViewModel/SettingVM.cs
@@ -23,6 +23,12 @@ namespace ProjectCycleManage.ViewModel
         [ObservableProperty]
         private Visibility _financialvisib = Visibility.Collapsed;
 
+        [ObservableProperty]
+        private Visibility _personalvisib = Visibility.Collapsed;
+
+        [ObservableProperty]
+        private Visibility _projectstagevisib = Visibility.Collapsed;
+
         #endregion
 
 
@@ -52,6 +58,29 @@ namespace ProjectCycleManage.ViewModel
             }
         }
 
+        private PersonManagVM _personalmanvm;
+        public PersonManagVM PersonManagVM
+        {
+            get => _personalmanvm;
+            set
+            {
+                _personalmanvm = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private ProjectStageVM _projectstagevm;
+        public ProjectStageVM ProjectstageviewVM
+        {
+            get => _projectstagevm;
+            set
+            {
+                _projectstagevm = value;
+                OnPropertyChanged();
+            }
+        }
+
         public SettingVM()
         {
             Mainareavisilib = Visibility.Visible;
@@ -69,7 +98,9 @@ namespace ProjectCycleManage.ViewModel
             ApprovalVilib = Visibility.Visible;
 
             Mainareavisilib = Visibility.Collapsed;
-
+            Personalvisib = Visibility.Collapsed;
+            Projectstagevisib = Visibility.Collapsed;
+            Financialvisib = Visibility.Collapsed;
             //MessageBox.Show("Amount Click!");
         }
 
@@ -80,7 +111,37 @@ namespace ProjectCycleManage.ViewModel
             Financialvisib = Visibility.Visible;
 
             Mainareavisilib = Visibility.Collapsed;
+            Personalvisib = Visibility.Collapsed;
+            Projectstagevisib = Visibility.Collapsed;
+            ApprovalVilib = Visibility.Collapsed;
         }
+
+
+        [RelayCommand]
+        private void PersonalVMCard()
+        {
+            PersonManagVM = new();
+            Personalvisib = Visibility.Visible;
+
+            ApprovalVilib = Visibility.Collapsed;
+            Financialvisib = Visibility.Collapsed;
+            Projectstagevisib = Visibility.Collapsed;
+            Mainareavisilib = Visibility.Collapsed;
+        }
+
+        [RelayCommand]
+        private void ProjectStageVMCard()
+        {
+            ProjectstageviewVM = new();
+            Projectstagevisib = Visibility.Visible;
+
+            ApprovalVilib = Visibility.Collapsed;
+            Financialvisib = Visibility.Collapsed;
+            Personalvisib = Visibility.Collapsed;
+            Mainareavisilib = Visibility.Collapsed;
+        }
+
+
 
         #endregion
 
