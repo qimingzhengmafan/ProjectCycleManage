7858e61bf80af00772b72710fa5fa699acd8cb86
DoctorWho <DoctorWho@bbc.com>
2025-12-01 18:30:11 +0800

    fix(视图): 修正人员管理视图中的工具提示和注释按钮
    将编辑按钮的工具提示改为"标记离职"，并注释掉角色和删除按钮的相关代码

--- ProjectCycleManage/ViewModel/ChartsVM.cs
+++ ProjectCycleManage/ViewModel/ChartsVM.cs
@@ -1,4 +1,4 @@
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.Kernel.Sketches;
